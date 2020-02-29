a0529c919dacc2f85f1ed02622f4cbff94730d82
rdbmighty <vinodatya@gmail.com>
2020-03-01 01:42:08 +0700

    profile progress

--- app/src/main/java/com/example/tongkrongin/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/tongkrongin/Activity/RegisterActivity.java
@@ -2,16 +2,20 @@ package com.example.tongkrongin.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.tongkrongin.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private ImageView logo;
     private EditText name,username,password;
     private Button signup,back;
 //    private TextView back;
@@ -21,6 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
 
+        logo = (ImageView)findViewById(R.id.logo);
 
         signup = (Button) findViewById(R.id.btn_Signup);
         signup.setOnClickListener(new View.OnClickListener() {
@@ -42,12 +47,20 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void Signup(){
         Intent intent = new Intent(this,LoginActivity.class);
-        startActivity(intent);
+        Pair[] pairs = new Pair[1];
+        pairs[0]=new Pair<View, String>(logo,"LogoTransition");
+
+        ActivityOptions option = ActivityOptions.makeSceneTransitionAnimation(this, pairs);
+        startActivity(intent,option.toBundle());
     }
 
     public void bACK(){
         Intent intent = new Intent(this,LoginActivity.class);
-        startActivity(intent);
+        Pair[] pairs = new Pair[1];
+        pairs[0]=new Pair<View, String>(logo,"LogoTransition");
+
+        ActivityOptions option = ActivityOptions.makeSceneTransitionAnimation(this, pairs);
+        startActivity(intent,option.toBundle());
     }
     }
 
